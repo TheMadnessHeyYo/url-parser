6f171d48477d6596aa5cd597e7205ef7a803f7ef
Matthew Cruz <matthewcruz.biz@gmail.com>
2018-06-26 23:18:39 -0400

    Added malformed boolean to track if url is detected as malformed.

--- url-parser.js
+++ url-parser.js
@@ -3,6 +3,11 @@ function parseUrl(url) {
 
 	var urlData = {}
 
+	// Store a flag to indicate if the url is malformed.
+	// Examples include a missing protocol.
+	urlData["malformed"] = false;
+
+
 	// Stash the entire url.
 	urlData["url"] = url;
 
