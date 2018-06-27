ff08813972d20b7e39894066d6ef3a2bea79476f
Matthew Cruz <matthewcruz.biz@gmail.com>
2018-06-27 01:46:02 -0400

    test.js: Converted to Class.

--- tests.js
+++ tests.js
@@ -1,60 +1,74 @@
+class Tests {
 
-// Include all tests that you want to run here.
-function runTests() {
-	console.log('----------------------------------------');
-	queryAndHashTests();
-	console.log('----------------------------------------');
-	queryTests();
-	console.log('----------------------------------------');
-	hashTests();
-	console.log('----------------------------------------');
-	wellformedTests();
-	console.log('----------------------------------------');
-	malformedTests();
-	console.log('----------------------------------------');
-}
+	constructor() {
 
-// Test Format:
-// - Use two separate console logs so that the object is inspectable in the browser dev-tools console.
+	}
 
-function queryAndHashTests() {
-	console.log('queryAndHashTests');
-	console.log(parseUrl('http://lsa.umich.edu/?first=charlie&last=brown#team=lions&city=detroit'));
-}
+	printSpacer() {
+		console.log('----------------------------------------');
+	}
 
-function queryTests() {
-	console.log('queryTests');
-	console.log(parseUrl('http://lsa.umich.edu/?first=charlie&last=brown'));
-}
+	// Include all tests that you want to run here.
+	runTests() {
+		this.printSpacer();
+		this.queryAndHashTests();
+		this.printSpacer();
+		this.queryTests();
+		this.printSpacer();
+		this.hashTests();
+		this.printSpacer();
+		this.wellformedTests();
+		this.printSpacer();
+		this.malformedTests();
+		this.printSpacer();
+	}
 
-function hashTests() {
-	console.log('hashTests');
-	console.log(parseUrl('http://lsa.umich.edu/#first=charlie&last=brown'));
-}
+	// Test Format:
+	// - Use two separate console logs so that the object is inspectable in the browser dev-tools console.
 
-function wellformedTests() {
-	console.log('wellformedTests');
+	queryAndHashTests() {
+		console.log('queryAndHashTests');
+		console.log(UrlParser.parseUrl('http://lsa.umich.edu/?first=charlie&last=brown#team=lions&city=detroit'));
+	}
 
-	console.log('http');
-	console.log(parseUrl('http://lsa.umich.edu/#first=charlie&last=brown'));
+	queryTests() {
+		console.log('queryTests');
+		console.log(UrlParser.parseUrl('http://lsa.umich.edu/?first=charlie&last=brown'));
+	}
 
-	console.log('https');
-	console.log(parseUrl('https://lsa.umich.edu/#first=charlie&last=brown'));
+	hashTests() {
+		console.log('hashTests');
+		console.log(UrlParser.parseUrl('http://lsa.umich.edu/#first=charlie&last=brown'));
+	}
 
-	console.log('file');
-	console.log(parseUrl('file://lsa.umich.edu/#first=charlie&last=brown'));
-}
+	wellformedTests() {
+		console.log('wellformedTests');
 
-function malformedTests() {
-	console.log('malformedTests');
+		console.log('http');
+		console.log(UrlParser.parseUrl('http://lsa.umich.edu/#first=charlie&last=brown'));
 
-	console.log('bad protocol');
-	console.log(parseUrl('httaps://lsa.umich.edu/#first=charlie&last=brown'));
+		console.log('https');
+		console.log(UrlParser.parseUrl('https://lsa.umich.edu/#first=charlie&last=brown'));
 
-	console.log('misplaced protocol');
-	console.log(parseUrl('lsa.umich.edu/http://#first=charlie&last=brown'));
+		console.log('file');
+		console.log(UrlParser.parseUrl('file://lsa.umich.edu/#first=charlie&last=brown'));
+	}
+
+	malformedTests() {
+		console.log('malformedTests');
+
+		console.log('bad protocol');
+		console.log(UrlParser.parseUrl('httaps://lsa.umich.edu/#first=charlie&last=brown'));
+
+		console.log('misplaced protocol');
+		console.log(UrlParser.parseUrl('lsa.umich.edu/http://#first=charlie&last=brown'));
+	}
+
+
 }
 
 
 // Execute tets.
-runTests();
+const tests = new Tests();
+tests.runTests();
+	
