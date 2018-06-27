f1477a3134223aadc2f405dc26c2b832180eeb4c
Matthew Cruz <matthewcruz.biz@gmail.com>
2018-06-27 00:39:25 -0400

    Changed object editing form from bracket to dot access.

--- url-parser.js
+++ url-parser.js
@@ -12,17 +12,17 @@ function parseUrl(url) {
 
 	// Store a flag to indicate if the url is malformed.
 	// Examples include a missing protocol.
-	urlData["malformed"] = false;
+	urlData.malformed = false;
 
 
 	// Stash the entire url.
-	urlData["url"] = url;
+	urlData.url = url;
 
 	
 	// Determine protocol.
 	var regex = /(^https?|^file):\/\//;
 	if (regex.test(url)) {
-		urlData["protocol"] = RegExp.lastMatch;
+		urlData.protocol = RegExp.lastMatch;
 	} else {
 		urlData.malformed = true;
 		return urlData;
