f9130d86fc7012f085c8542f37a8c7de6231fda5
Matthew Cruz <matthewcruz.biz@gmail.com>
2018-06-27 00:40:07 -0400

    Discovered built-in URL object and used it to help parse query and hash params.

--- url-parser.js
+++ url-parser.js
@@ -28,5 +28,27 @@ function parseUrl(url) {
 		return urlData;
 	}
 
+	
+	// Use URL object to parse query and hash parameters if present.
+	var urlObject = new URL(url);
+
+	if (urlObject.search.length) {
+		var paramsString = urlObject.search.slice(1); // Slice to remove "?" from start.
+		var keyValueStrings = paramsString.split('&'); // Turn into an array of key=value strings.
+		for (var i=0; i < keyValueStrings.length; i++) {
+			var keyValue = keyValueStrings[i].split('='); // Turn string into array [key, value].
+			urlData.queryParams[keyValue[0]] = keyValue[1]; // Assign key and value to urlData object.
+		}
+	}
+
+	if (urlObject.hash.length) {
+		var paramsString = urlObject.hash.slice(1); // Slice to remove "#" from start.
+		var keyValueStrings = paramsString.split('&'); // Turn into an array of key=value strings.
+		for (var i=0; i < keyValueStrings.length; i++) {
+			var keyValue = keyValueStrings[i].split('='); // Turn string into array [key, value].
+			urlData.hashParams[keyValue[0]] = keyValue[1]; // Assign key and value to urlData object.
+		}
+	}
+
 	return urlData;
 }
