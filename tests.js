568a17c18505af6ab3ccef43ec2e4b590a9c395f
Matthew Cruz <matthewcruz.biz@gmail.com>
2018-06-26 23:22:39 -0400

    wellformedTests: Reformatted and added three tests.

--- tests.js
+++ tests.js
@@ -4,10 +4,20 @@ function runTests() {
 	wellformedTests();
 }
 
+// Test Format:
+// - Use two separate console logs so that the object is inspectable in the browser dev-tools console.
 
 function wellformedTests() {
 	console.log("wellformedTests");
+
+	console.log('http');
+	console.log(parseUrl("http://lsa.umich.edu/#q=physics&start=0&hasnext=true&mode=gsa&site=LSA"));
+
+	console.log('https');
 	console.log(parseUrl("https://lsa.umich.edu/#q=physics&start=0&hasnext=true&mode=gsa&site=LSA"));
+
+	console.log('file');
+	console.log(parseUrl("file://lsa.umich.edu/#q=physics&start=0&hasnext=true&mode=gsa&site=LSA"));
 }
 
 
