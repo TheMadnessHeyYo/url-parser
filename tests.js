20e6f09b2a74e61935c83f0797f2136da8531e00
Matthew Cruz <matthewcruz.biz@gmail.com>
2018-06-27 00:36:31 -0400

    wellformedTests: Modified to use cleaner params for testing.

--- tests.js
+++ tests.js
@@ -12,16 +12,16 @@ function runTests() {
 // - Use two separate console logs so that the object is inspectable in the browser dev-tools console.
 
 function wellformedTests() {
-	console.log("wellformedTests");
+	console.log('wellformedTests');
 
 	console.log('http');
-	console.log(parseUrl("http://lsa.umich.edu/#q=physics&start=0&hasnext=true&mode=gsa&site=LSA"));
+	console.log(parseUrl('http://lsa.umich.edu/#first=charlie&last=brown'));
 
 	console.log('https');
-	console.log(parseUrl("https://lsa.umich.edu/#q=physics&start=0&hasnext=true&mode=gsa&site=LSA"));
+	console.log(parseUrl('https://lsa.umich.edu/#first=charlie&last=brown'));
 
 	console.log('file');
-	console.log(parseUrl("file://lsa.umich.edu/#q=physics&start=0&hasnext=true&mode=gsa&site=LSA"));
+	console.log(parseUrl('file://lsa.umich.edu/#first=charlie&last=brown'));
 }
 
 function malformedTests() {
