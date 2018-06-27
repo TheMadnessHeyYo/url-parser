0accbf9846d5418a64bd082183619376bc663668
Matthew Cruz <matthewcruz.biz@gmail.com>
2018-06-26 23:21:00 -0400

    Renamed test1 to wellformedTests.

--- tests.js
+++ tests.js
@@ -1,13 +1,12 @@
 
 // Include all tests that you want to run here.
 function runTests() {
-	test1();
+	wellformedTests();
 }
 
 
-// Basic test.
-function test1() {
-	console.log("Test1"); // Doing two separate console logs so that the object is inspectable in the browser dev-tools console.
+function wellformedTests() {
+	console.log("wellformedTests");
 	console.log(parseUrl("https://lsa.umich.edu/#q=physics&start=0&hasnext=true&mode=gsa&site=LSA"));
 }
 
