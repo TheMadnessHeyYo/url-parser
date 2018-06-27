bd6d7d50d5162c0b82c6474ba7daca37d0230db8
Matthew Cruz <matthewcruz.biz@gmail.com>
2018-06-27 00:57:29 -0400

    Revert "Removed redundant protocol determination and consolidated object access."
    This reverts commit c3eaca6ecb859a8a60bbb26f2b28e80a71830716.
    
    It turns out that the URL object fails to capture a malformed url that begins with “httaps”, so I am going to reinstate the regex protocol inspection.

--- url-parser.js
+++ url-parser.js
@@ -3,25 +3,37 @@ function parseUrl(url) {
 
 	// Create return object with default key-value pairs.
 	var urlData = {
-		url: url,			// Stash the entire url string.
-		urlObject: null,	// Will contain pointer to built-in URL object.
-		malformed: false,	// true indicates the url is malformed.
-		queryParams: {},	// Holds the query params key-values if present.
-		hashParams: {}		// Holds the hash params key-values if present.
+		malformed: false,
+		url: "",
+		protocol: "",
+		queryParams: {},
+		hashParams: {}
 	}
 
-	// Use built-in URL object to preprocess url. This will obtain the protocol, hash, and query params + more.
-	// Use try-catch because the URL object will fail at creation if the url is malformed.
-	try {
-		urlData.urlObject = new URL(url)
-	} catch(error) {
+	// Store a flag to indicate if the url is malformed.
+	// Examples include a missing protocol.
+	urlData.malformed = false;
+
+
+	// Stash the entire url.
+	urlData.url = url;
+
+	
+	// Determine protocol.
+	var regex = /(^https?|^file):\/\//;
+	if (regex.test(url)) {
+		urlData.protocol = RegExp.lastMatch;
+	} else {
 		urlData.malformed = true;
 		return urlData;
 	}
-		
-	// Parse query and hash parameters if present into easy to access objects.
-	if (urlData.urlObject.search.length) {
-		var paramsString = urlData.urlObject.search.slice(1); // Slice to remove "?" from start.
+
+	
+	// Use URL object to parse query and hash parameters if present.
+	var urlObject = new URL(url);
+
+	if (urlObject.search.length) {
+		var paramsString = urlObject.search.slice(1); // Slice to remove "?" from start.
 		var keyValueStrings = paramsString.split('&'); // Turn into an array of key=value strings.
 		for (var i=0; i < keyValueStrings.length; i++) {
 			var keyValue = keyValueStrings[i].split('='); // Turn string into array [key, value].
@@ -29,8 +41,8 @@ function parseUrl(url) {
 		}
 	}
 
-	if (urlData.urlObject.hash.length) {
-		var paramsString = urlData.urlObject.hash.slice(1); // Slice to remove "#" from start.
+	if (urlObject.hash.length) {
+		var paramsString = urlObject.hash.slice(1); // Slice to remove "#" from start.
 		var keyValueStrings = paramsString.split('&'); // Turn into an array of key=value strings.
 		for (var i=0; i < keyValueStrings.length; i++) {
 			var keyValue = keyValueStrings[i].split('='); // Turn string into array [key, value].
